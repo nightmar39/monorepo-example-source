8ae3ada1619cb77cb1a9612fbe375758ae5b298c
Ted Spinks <ted.spinks@codefresh.io>
2022-06-21 10:30:29 -0400

    Accommodate ingress URL rewriting

--- color/05-interpolation.sh
+++ color/05-interpolation.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-sed -i "s/COLOR1/${COLOR1}/g" /usr/share/nginx/html/index.html
-sed -i "s/COLOR2/${COLOR2}/g" /usr/share/nginx/html/index.html
-sed -i "s/COLOR3/${COLOR3}/g" /usr/share/nginx/html/index.html
-sed -i "s/IMAGE/${IMAGE}/g" /usr/share/nginx/html/index.html
+sed -i "s+COLOR1+${COLOR1}+g" /usr/share/nginx/html/index.html
+sed -i "s+COLOR2+${COLOR2}+g" /usr/share/nginx/html/index.html
+sed -i "s+COLOR3+${COLOR3}+g" /usr/share/nginx/html/index.html
+sed -i "s+IMAGE+${IMAGE}+g" /usr/share/nginx/html/index.html
 
 cat /usr/share/nginx/html/index.html
 
