e6524c249a6e31a8a9a5a351c12fa6feda389464
Dan Amzulescu <dan.amzulescu@codefresh.io>
2022-06-23 14:11:16 -0500

    new version

--- color/05-interpolation.sh
+++ color/05-interpolation.sh
@@ -3,8 +3,7 @@
 set -e
 
 sed -i "s+COLOR1+${COLOR1}+g" /usr/share/nginx/html/index.html
-sed -i "s+COLOR2+${COLOR2}+g" /usr/share/nginx/html/index.html
-sed -i "s+COLOR3+${COLOR3}+g" /usr/share/nginx/html/index.html
+
 sed -i "s+IMAGE+${IMAGE}+g" /usr/share/nginx/html/index.html
 
 cat /usr/share/nginx/html/index.html
